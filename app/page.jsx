dd6a53bddd7b424ffa60ec8727c9e1b9e4463903
ali-hasnain-rapidzz <ali.hasnain@rapidzzsolutions.com>
2026-01-13 13:56:43 +0500

    updated meta title, meta description for Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
 import HomeClient from "@/components/home/HomeClient";
 
 export const metadata = {
-  title: "Buy, Sell & Rent Properties in Dubai | Earlybird",
-  description: "Find luxury villas, apartments & commercial spaces in Dubai. Buy, sell or rent properties easily with Earlybird Properties.",
+  title: "Dubai Real Estate | Buy Apartments – Earlybirds",
+  description: "Find the best properties in Dubai – apartments, villas, townhouses & off-plan listings. Trusted real estate agency helping buyers & investors. Contact us!",
   verification: {
     google: "0OhWG1sDYTWm5afG1TBmeIeVq0U-5tt8KYxZh6HiUps",
   },
